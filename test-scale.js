0d4bc95c4e0c87d5b35f8044927823fa4bc4445e
I Made Aris Danuarta <arisftp2@gmail.com>
2026-09-02 13:10:53 +0800

    chore: remove obsolete scratchpad, test, and maintenance scripts

--- test-scale.js
+++
@@ -1,7 +0,0 @@
-import { geoMercator } from "d3-geo";
-import fs from "fs";
-
-const geojson = JSON.parse(fs.readFileSync("apps/web/public/data/IDN_adm_1_provinsi.json"));
-const projection = geoMercator().fitSize([1000, 800], geojson);
-console.log("Scale:", projection.scale());
-console.log("Center:", projection.invert(projection.translate()));
